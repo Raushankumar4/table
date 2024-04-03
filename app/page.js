fdc633d0dd814ea7d4d2481a14db7abd614fd3fa
Raushan Kumar <raushankumarguptag@gmail.com>
2024-04-03 11:44:12 +0530

    table

---
+++ app/page.js
@@ -0,0 +1,15 @@
+"use client";
+import React from "react";
+import Container from "@/Components/Container";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <>
+      <Link href="/"></Link>
+      <Container />
+    </>
+  );
+};
+
+export default page;
